d1f8863c8705ed8238f7745df329215d7b1f48f8
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-18 01:38:15 +0600

    implement isKeyword function

--- keyword-check.c
+++ keyword-check.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 const char keywords[][] = {
     "auto", "break", "case", "char", "const", "continue", "default", "do",
     "double", "else", "enum", "extern", "float", "for", "goto", "if",
@@ -5,3 +7,11 @@ const char keywords[][] = {
     "static", "struct", "switch", "typedef", "union", "unsigned",
     "void", "volatile", "while"
 };
+int isKeyword(const char *str) {
+    for (int i = 0; i < 32; i++) {
+        if (strcmp(str, keywords[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
