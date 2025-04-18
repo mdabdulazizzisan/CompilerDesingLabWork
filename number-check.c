d664bbdf7478c9ba13f3813b67187070583ec259
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-18 07:38:44 +0600

    implement isNumber function in number-check.

---
+++ number-check.c
@@ -0,0 +1,10 @@
+#include <string.h>
+
+int isNumber(const char *str) {
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] < '0' || str[i] > '9') {
+            return 0; // Not a number
+        }
+    }
+    return 1; // Is a number
+}
