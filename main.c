1875802c383b3093205a0053be8e84f3a47965c2
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-18 09:07:02 +0600

    implement checking all the functions at once.

--- main.c
+++ main.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 #include "identifier-check.c"
+#include "keyword-check.c"
+#include "number-check.c"
+#include "operator-check.c"
 
 int main(void) {
     char input[100];
-    printf("Enter a name as identifier: ");
+    int criteria_met = 0;  // Flag to track how many criteria is met
+    
+    printf("Enter a string to check: ");
     scanf("%s", input);
-    if (!isIdentifier(input)) {
-        printf("%s is not an identifier.\n %c is an invalid character at the beginning of an identifier.\n", input, input[0]);
-    } else if (isIdentifier(input) == 2) {
-        printf("%s is a valid identifier.\n", input);
-    } else {
-        printf("%s is not a valid identifier.\n", input);
+    
+    // 1. Check if input is a keyword
+    if (isKeyword(input)) {
+        printf("'%s' is a C keyword\n", input);
+        criteria_met++;
     }
+    
+    // 2. Check if input is a valid identifier
+    if (isIdentifier(input)) {
+        printf("'%s' is a valid identifier\n", input);
+        criteria_met++;
+    }
+    
+    // 3. Check if input is a number
+    if (isNumber(input)) {
+        printf("'%s' is a number\n", input);
+        criteria_met++;
+    }
+    
+    // 4. Check if input is an operator
+    if (strlen(input) == 1 && isOperator(input[0])) {
+        printf("'%s' is an operator\n", input);
+        criteria_met++;
+    }
+    
+    if (!criteria_met) {
+        printf("'%s' doesn't fit any of the specified criteria\n", input);
+    }else {
+        printf("The string fits %d of the specified criteria\n", criteria_met);
+    }
+    
     return 0;
 }
