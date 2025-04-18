0bfdb9bc05af5af12ccc46dbffcab322d4d42559
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-18 08:18:55 +0600

    testing from main funtion

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include "number-check.c"
+#include "identifier-check.c"
 
 int main(void) {
-    char input[20];
-    printf("Enter a number: ");
+    char input[100];
+    printf("Enter a name as identifier: ");
     scanf("%s", input);
-    if (isNumber(input)) {
-        printf("%s is a number.\n", input);
+    if (!isIdentifier(input)) {
+        printf("%s is not an identifier.\n %c is an invalid character at the beginning of an identifier.\n", input, input[0]);
+    } else if (isIdentifier(input) == 2) {
+        printf("%s is a valid identifier.\n", input);
     } else {
-        printf("%s is not a number.\n", input);
+        printf("%s is not a valid identifier.\n", input);
     }
     return 0;
 }
